cc7eebf5a4c69f6db9766166a43e34a9f3fc77d2
Ddinata <aviscerality@gmail.com>
2019-02-21 00:58:58 -0800

    yo

--- routes/addlisting.js
+++ routes/addlisting.js
@@ -6,20 +6,17 @@ exports.view = function(req, res){
 
 exports.addSeller = function(req, res) {
   var fs = require('fs');
-  var sellers = require('./../public/data/sellers.json');
-  listing = {}
-  sellers.push(listing)
-  var jsonData = JSON.stringify(sellers);
-  fs.writeFile('./../public/data/sellers.json', jsonData, function(err) {
-      if (err) {
-          console.log(err);
-      }
-  });
-
-
-  console.log(sellers);
+  var listings = require('./../public/data/listings.json');
+  listing = {};
+  listings["sellers"].push(listing);
+  // var jsonData = JSON.stringify(listings);
+  // fs.writeFile('./../public/data/listings.json', jsonData, function(err) {
+  //     if (err) {
+  //         console.log(err);
+  //     }
+  // });
+  console.log(listings["sellers"]);
   console.log(req.body.single);
-
   res.end();
 }
 
